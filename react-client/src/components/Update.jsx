3de9b2d7b7de168c25ed4414edd93cb69690edc7
Mohamed Alaya <alayamohamed@outlook.com>
2022-01-02 21:26:55 +0100

    update frontend working

--- react-client/src/components/Update.jsx
+++ react-client/src/components/Update.jsx
@@ -1,35 +1,35 @@
 import React from"react";
 
+const Update =({handleChange, handleUpdate, currentproduct})=> (
 
-const Update = ({ currentproduct, handleChange, handleUpdate }) => (
-  <div className="update">
-    <div>   {currentproduct.name}  - {currentproduct.type} - {currentproduct.inventory} - {currentproduct.price} - {currentproduct.note}    </div>
-    <form>
-    <div>
-    <label htmlFor="name">  New product's Name  : </label>
-      <input type="text" id='name' value={currentproduct.name} onChange={handleChange} ></input>
-    </div>
-    <div>
-    <label htmlFor="Type ">  New product's Type  : </label>
-      <input type="text" id='type' value={currentproduct.type} onChange={handleChange} ></input>
-    </div>
-    <div>
-    <label htmlFor="stock ">  New product's Stock  : </label>
-      <input type="number" id='stock' value={currentproduct.inventory} onChange={handleChange} ></input>
-    </div>
-    <div>
-    <label htmlFor="price ">  New product's Price : </label>
-      <input type="number" id='price' value={currentproduct.price} onChange={handleChange} ></input>
-    </div>
-    <div>
-    <label htmlFor="Type ">  New product's Note : </label>
-      <input type="text" id='note' value={currentproduct.note} onChange={handleChange} ></input>
-    </div>
-    </form>
-    <div>
-      <button className='updateClass' type='submit' onClick={handleUpdate}> Update this product </button>
-    </div>
-  </div>
-);
+	<div className="create">
+		<div>{currentproduct.name}--{currentproduct.type}--{currentproduct.price}--{currentproduct.inventory}--{currentproduct.note}</div>
+	<div>
+		<label htmlFor="name">Name</label><br />
+		<input type="text" name='name' onChange={handleChange} required />
+	</div>
+	<div>
+		<label htmlFor="name">Type</label><br />
+		<input type="text" name='type' onChange={handleChange} required />
+	</div>
+	<div>
+		<label htmlFor="name">Price</label><br />
+		<input type="number" name='price' onChange={handleChange} required />
+	</div>
+	<div>
+		<label htmlFor="name">Inventory</label><br />
+		<input type="number" name='inventory' onChange={handleChange} required />
+	</div>
+	<div>
+		<label htmlFor="name">Note</label><br />
+		<input type="text" name='note' onChange={handleChange} required />
+	</div>
 
-export default Update ;
+
+
+		<button type='submit' onClick={handleUpdate}>submit</button>
+
+	</div>
+	)
+
+	export default Update
